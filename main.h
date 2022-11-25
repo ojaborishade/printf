d8cfcfe214b42c016b551c1383510c6cb73ff22b
Olufunmilayo <olufunmilayoj.aborishade@gmail.com>
2022-11-25 00:55:35 -0800

    main.h

--- main.h
+++ main.h
@@ -17,7 +17,31 @@
 
 /* SIZES */
 #define S_LONG 2
+#define S_SHORT 1
+
+/**
+ * struct fmt - struct op
+ * @fmt: The format
+ * @fn: function associated
+ */
+
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int);
+};
+
+
+/**
+ * typedef struct fmt fmt_t - Struct op
+ * @fmt: The Format
+ * @fm_t: Fuction associated
+ */
+
+typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
 
 #endif
